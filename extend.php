006cc3915d3de41527c7fda0bfcd7e4ef161b544
Eddie Webbinaro <ollitech@gmail.com>
2024-11-21 18:50:29 -0500

    remove frontend assets

--- extend.php
+++ extend.php
@@ -14,13 +14,6 @@ namespace Webbinaro\BlueskyHandles;
 use Flarum\Extend;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/less/admin.less'),
-    new Extend\Locales(__DIR__.'/locale'),
     (new Extend\Routes('api'))
         ->get('/bluesky/{slug}', 'bluesky.did', Routes\BlueskyController::class),
 ];
